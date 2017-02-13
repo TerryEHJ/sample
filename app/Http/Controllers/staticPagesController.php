b472c4465a8e6f849e69919cf65fd3c0d263ec9c
Terry Yi <terry.ehj@gmail.com>
2017-02-13 03:16:03 +0000

    Finish layout and routes

--- app/Http/Controllers/staticPagesController.php
+++ app/Http/Controllers/staticPagesController.php
@@ -11,16 +11,16 @@ class staticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        return view('static_pages.home');
     }
 
     public function help()
     {
-        return view('static_pages/help');
+        return view('static_pages.help');
     }
 
     public function about()
     {
-        return view('static_pages/about');
+        return view('static_pages.about');
     }
 }

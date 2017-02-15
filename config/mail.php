e4906b3a507f3fc29d368cd9c7d4476583adab11
Terry Yi <terry.ehj@gmail.com>
2017-02-15 08:43:44 +0000

    add mail address and name

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'terry.ehj@gmail.com', 'name' => 'Terry Yi'],
 
     /*
     |--------------------------------------------------------------------------

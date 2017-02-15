69406a18f82efce756dc2b5bfa19b138ae1f4354
Terry Yi <terry.ehj@gmail.com>
2017-02-15 08:01:18 +0000

    Add password resets & email configuration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,23 @@
 |
 */
 
+//静态页面
 get('/', 'StaticPagesController@home')->name('home');
 get('/help', 'StaticPagesController@help')->name('help');
 get('/about', 'StaticPagesController@about')->name('about');
 
+//用户操作
 get('signup', 'UsersController@create')->name('signup');
 resource('users', 'UsersController');
+get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+//登录登出
 get('login', 'SessionsController@create')->name('login');
 post('login', 'SessionsController@store')->name('login');
 delete('login', 'SessionsController@destroy')->name('logout');
 
-get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+//重置密码
+get('password/email', 'Auth\PasswordController@getEmail')->name('password.reset');
+post('password/email', 'Auth\PasswordController@postEmail')->name('password.reset');
+get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');

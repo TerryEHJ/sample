df31c7e389f928361b0434fc71a380404bac4be8
Terry Yi <terry.ehj@gmail.com>
2017-02-23 16:39:21 +0800

    fix database and mail config

--- config/mail.php
+++ config/mail.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'terry.ehj@gmail.com', 'name' => 'Terry Yi'],
+    'from' => ['address' => null, 'name' => null],
 
     /*
     |--------------------------------------------------------------------------
